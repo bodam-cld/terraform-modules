8c94e27aaf5afb28f049e8d925decd347c78c46f
Bela Pesics <bela.pesics@zenheads.hu>
2022-03-22 19:18:20 +0100

    move deployment bucket output

--- core-aws-main/outputs.tf
+++ core-aws-main/outputs.tf
@@ -1,9 +1,3 @@
 output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
-
-output "deployment_bucket" {
-  value = {
-    s3_deployment_bucket = aws_s3_bucket.deployment.id
-  }
-}
--- core-aws/outputs.tf
+++ core-aws/outputs.tf
@@ -8,3 +8,9 @@ output "aws_route53_technical_domain" {
 output "route53_zone_name_servers" {
   value = aws_route53_zone.this.name_servers
 }
+
+output "deployment_bucket" {
+  value = {
+    s3_deployment_bucket = aws_s3_bucket.deployment.id
+  }
+}
