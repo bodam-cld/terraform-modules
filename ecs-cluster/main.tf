b2b25824f9b1191bbacbcf95b26145cdcb05e7a8
Janos Krnak <janos.krnak@gmail.com>
2022-04-01 08:06:04 +0200

    disable tfsec aws-ecs-enable-container-insight warning

--- ecs-cluster/main.tf
+++ ecs-cluster/main.tf
@@ -2,7 +2,8 @@ resource "aws_ecs_cluster" "this" {
   name = var.name
 
   setting {
-    name  = "containerInsights"
+    name = "containerInsights"
+    #tfsec:ignore:aws-ecs-enable-container-insight => container insights results in extra metrics that costs money, let this decided
     value = var.enable_container_insights ? "enabled" : "disabled"
   }
 }
