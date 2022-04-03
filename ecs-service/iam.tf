5e74017b285c0ac86db83dc3453466c83a07b18d
Janos Krnak <janos.krnak@gmail.com>
2022-04-03 17:11:12 +0200

    feat: ecs-service, currently only fargate is supported

---
+++ ecs-service/iam.tf
@@ -0,0 +1,99 @@
+# Task role
+data "aws_iam_policy_document" "assume_role_ecs" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "task_role" {
+  name               = "${var.environment}-${var.service_name}-ecs-task"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_ecs.json
+}
+
+# Task execution role
+data "aws_iam_policy_document" "task_execution_role" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["${aws_cloudwatch_log_group.this.arn}:*"]
+  }
+
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    ]
+
+    resources = [aws_ecr_repository.this.arn]
+  }
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${var.environment}-${var.service_name}-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_ecs.json
+}
+
+resource "aws_iam_role_policy" "task_execution_role" {
+  name   = "execution-policy"
+  role   = aws_iam_role.task_execution_role.name
+  policy = data.aws_iam_policy_document.task_execution_role.json
+}
+
+data "aws_iam_policy_document" "ssm_and_secrets" {
+  statement {
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath"
+    ]
+
+    resources = ["arn:aws:ssm:*:${data.aws_caller_identity.current.account_id}:parameter/${var.environment}/${var.service_name}/*"]
+  }
+
+  statement {
+    actions = [
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetSecretValue",
+    ]
+
+    resources = ["arn:aws:secretsmanager:*:${data.aws_caller_identity.current.account_id}:secret:/${var.environment}/${var.service_name}/*"]
+  }
+
+
+  statement {
+    actions = [
+      "kms:Decrypt"
+    ]
+
+    resources = ["arn:aws:kms:*:${data.aws_caller_identity.current.account_id}:key/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/ServiceName"
+      values   = [var.service_name]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "ssm_and_secrets" {
+  name   = "ssm-and-secret-access"
+  role   = aws_iam_role.task_execution_role.name
+  policy = data.aws_iam_policy_document.ssm_and_secrets.json
+}
