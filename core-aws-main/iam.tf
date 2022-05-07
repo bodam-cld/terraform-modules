7b0ec2efbc604e17075585406a22d36661898c1a
Janos Krnak <janos.krnak@gmail.com>
2022-05-07 19:38:09 +0200

    feat: deployer user in the main account

--- core-aws-main/iam.tf
+++ core-aws-main/iam.tf
@@ -33,3 +33,25 @@ module "iam_assume_groups" {
   name            = each.key
   assumable_roles = each.value.roles
 }
+
+# CI deployer user
+resource "aws_iam_user" "ci_deployer" {
+  name = "ci-deployer"
+}
+
+resource "aws_iam_access_key" "ci_deployer" {
+  user = aws_iam_user.ci_deployer.name
+}
+
+data "aws_iam_policy_document" "ci_deployer_assume_policies" {
+  statement {
+    actions   = ["sts:AssumeRole"]
+    resources = var.ci_deployer_role_arns
+  }
+}
+
+resource "aws_iam_user_policy" "ci_deployer_assume" {
+  name   = "assume-policies"
+  user   = aws_iam_user.ci_deployer.name
+  policy = data.aws_iam_policy_document.ci_deployer_assume_policies.json
+}
