d3e2c39d7703f413710247cc8e7ba9317ec762e0
Janos Krnak <janos.krnak@gmail.com>
2022-05-09 11:18:08 +0200

    change output name for ci deployer keys

--- core-aws-main/outputs.tf
+++ core-aws-main/outputs.tf
@@ -2,7 +2,7 @@ output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 
-output "aws_iam_user_keys" {
+output "ci_deployer_keys" {
   value = {
     iam_user_arn          = aws_iam_user.ci_deployer.arn
     iam_access_key        = aws_iam_access_key.ci_deployer.id
